68f2c8a9a130a3975e3289bc8a152ef33aafbcc1
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-02-12 16:00:08 +0900

    Add new styles and variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-      host: true,
-      open: true,
+  server: {
+    host: true,
+    open: true,
   },
   site: 'https://yuheijotaki.com',
   integrations: [mdx(), sitemap()],

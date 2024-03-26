c9306ebe8729d379a7d04fd697335488c25a4db4
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-03-26 12:56:12 +0900

    Update key

--- tina/config.ts
+++ tina/config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'tinacms';
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || 'main';
+const branch = 'main';
 
 export default defineConfig({
   branch,
-
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.TINA_CLIENT_ID,
   // Get this from tina.io
   token: process.env.TINA_TOKEN,
 

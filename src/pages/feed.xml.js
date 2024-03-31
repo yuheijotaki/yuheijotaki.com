9aa876dc6db138a3753fa02332d3acf1a1e1df04
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-03-31 20:48:09 +0900

    Fix feed.xml

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,24 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+import { SITE_TITLE, SITE_DESCRIPTION } from '@/consts';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const allPosts = (await getCollection('blog')).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
+  const feedPosts = allPosts.slice(0, 10);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: feedPosts.map((post) => ({
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
+    })),
+    customData: `<language>ja</language>`,
+  });
+}

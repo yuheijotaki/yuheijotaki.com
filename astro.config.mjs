ba82d8d37b5be311f1bf520e456ef7ac8c2e6c17
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-04-30 15:22:59 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import swup from '@swup/astro';
+// import swup from '@swup/astro';
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -23,23 +23,23 @@ export default defineConfig({
     react(),
     mdx(),
     sitemap(),
-    swup({
-      theme: false,
-      // animationClass: 'transition-',
-      // containers: ['main'] // セレクタ選択できるが指定すると動かなかったためHTML要素 `.transition-fade` を指定
-      cache: true,
-      preload: {
-        hover: true,
-        visible: false, // ビューポートに入るときにプリロードすると記事一覧で重くなるためfalseに
-      },
-      accessibility: true, // フォーカスが移動しないためtrueにするが、トップではロゴにフォーカス移動したほうがよさげなので要調整？
-      progress: false,
-      smoothScrolling: false,
-      globalInstance: true,
-      // updateBodyClass: true,
-      // debug: true,
-      // reloadScripts: false, // 遷移時にscriptを再読み込みさせない
-    }),
+    // swup({
+    //   theme: false,
+    //   // animationClass: 'transition-',
+    //   // containers: ['main'] // セレクタ選択できるが指定すると動かなかったためHTML要素 `.transition-fade` を指定
+    //   cache: true,
+    //   preload: {
+    //     hover: true,
+    //     visible: false, // ビューポートに入るときにプリロードすると記事一覧で重くなるためfalseに
+    //   },
+    //   accessibility: true, // フォーカスが移動しないためtrueにするが、トップではロゴにフォーカス移動したほうがよさげなので要調整？
+    //   progress: false,
+    //   smoothScrolling: false,
+    //   globalInstance: true,
+    //   // updateBodyClass: true,
+    //   // debug: true,
+    //   // reloadScripts: false, // 遷移時にscriptを再読み込みさせない
+    // }),
     icon({
       iconDir: 'src/icons',
     }),

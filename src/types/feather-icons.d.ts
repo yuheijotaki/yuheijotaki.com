fb8de8a9f398fbb72cc01060afad116a95fa2812
Yuhei Jotaki <yuhei.jotaki@trq.co.jp>
2024-11-01 18:24:18 +0900

    fix:

--- src/types/feather-icons.d.ts
+++ src/types/feather-icons.d.ts
@@ -3,7 +3,7 @@ declare module 'feather-icons' {
   const feather: {
     icons: {
       [key: string]: {
-        toSvg: (options?: { [key: string]: any }) => string;
+        toSvg: () => string;
       };
     };
   };
